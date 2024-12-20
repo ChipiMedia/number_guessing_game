4e177b0e6a48c895ab2a98c8e248a779c1bd795b
Chipi Media <chipi989@outlook.com>
2024-12-20 15:55:15 +0000

    feat: generate and display secret number

--- number_guess.sh
+++ number_guess.sh
@@ -15,3 +15,7 @@ else
   IFS="|" read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_RECORD"
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+SECRET_NUMBER=$((RANDOM % 1000 + 1))
+echo "Guess the secret number between 1 and 1000: "
+
