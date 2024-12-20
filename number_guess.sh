0691368d69fcc94e77c358173212c14d56b8b41b
Chipi Media <chipi989@outlook.com>
2024-12-20 15:56:14 +0000

    feat: add guessing game logic

--- number_guess.sh
+++ number_guess.sh
@@ -19,3 +19,25 @@ fi
 SECRET_NUMBER=$((RANDOM % 1000 + 1))
 echo "Guess the secret number between 1 and 1000: "
 
+NUMBER_OF_GUESSES=0
+
+while true
+do
+  read GUESS
+
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again: "
+    continue
+  fi
+
+  (( NUMBER_OF_GUESSES++ ))
+
+  if [[ $GUESS -eq $SECRET_NUMBER ]]; then
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    break
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]; then
+    echo "It's lower than that, guess again: "
+  else
+    echo "It's higher than that, guess again: "
+  fi
+done
